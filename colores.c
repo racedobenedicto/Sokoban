6d6b064bd4afbb0aeec13a1a1fd949d183b82345
racedobenedicto <74828495+racedobenedicto@users.noreply.github.com>
2020-11-24 09:42:06 +0100

    Create colores.c

---
+++ colores.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "colores.h"
+
+// #define ANSI_COLOR_BLACK   "\x1b[30m"
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_WHITE   "\x1b[37m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_BOLD    "\x1b[1m"
+
+void printf_color(int color) {
+	switch (color % 14) {
+		case RED:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_RED    ); break;
+		case GREEN:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_GREEN  ); break;
+		case YELLOW:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_YELLOW ); break;
+		case BLUE:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_BLUE   ); break;
+		case MAGENTA:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_MAGENTA); break;
+		case CYAN:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_CYAN   ); break;
+		case WHITE:
+			printf(ANSI_COLOR_RESET  ); printf(ANSI_COLOR_WHITE  ); break;
+		case BOLD_RED:
+			printf(ANSI_COLOR_BOLD   ); printf(ANSI_COLOR_RED    ); break;
+		case BOLD_GREEN:
